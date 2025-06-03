0ecc8eeb9ddc08d6135bdfc8b614cedd1c4f1d6c
jose <joshernan.ferrari@gmail.com>
2025-06-02 23:45:16 -0400

    cargada la base de datos

---
+++ trigger.sql
@@ -0,0 +1,17 @@
+
+DELIMITER $$
+
+CREATE DEFINER=`jh`@`localhost` TRIGGER asignar_juegos_iniciales
+AFTER INSERT ON tb_Jugadores
+FOR EACH ROW
+BEGIN
+    INSERT INTO tb_Juegos_Jugadores (ID_Juego, ID_Jugador, EsCompra)
+    VALUES
+        (1, NEW.ID_Jugador, 0),
+        (2, NEW.ID_Jugador, 0),
+        (3, NEW.ID_Jugador, 0),
+        (4, NEW.ID_Jugador, 0);
+END$$
+
+DELIMITER ;
+
